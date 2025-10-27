a1aaef95cfd68b711bae55527d05aaa0a536383a
a4827909 <a4827909@sina.com>
2025-10-27 13:41:56 +0800

    合约开发实战，任务一

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - **go作业**：go_homework_project
 - **solidity作业**：solidity_homework_project
 - **DAPP作业**：DAPP_homework_project
+- **合约开发实战作业**：contract_dev_homework_project
 
 
 
---
+++ contract_dev_homework_project/taskOne/hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      viaIR: true, // 关键：启用 IR 编译流程，缓解栈过深
+    },
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
+};
